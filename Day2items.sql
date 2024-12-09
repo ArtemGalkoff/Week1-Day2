e712c05b8f8488f11691dd2e910d0e7df2866c0a
Artem Galkov <aegalkov@gmail.com>
2024-12-09 21:01:42 +0200

    Add files via upload

---
+++ Day2items.sql
@@ -0,0 +1,7 @@
+-- SELECT * FROM items WHERE price > 80;
+-- SELECT * FROM items WHERE price <= 300;
+-- SELECT * FROM customers WHERE customer_sername = 'Smith';
+-- SELECT * FROM customers WHERE customer_sername = 'Jones';
+-- SELECT * FROM customers WHERE customer_sername != 'Jones';
+
+
---
+++ day2students.sql
@@ -0,0 +1,16 @@
+-- SELECT id FROM students WHERE first_name = 'Galkov'
+-- SELECT * FROM students
+-- SELECT first_name, last_name FROM students
+-- SELECT * FROM students WHERE id = 4
+-- SELECT * FROM students WHERE first_name = 'Benichou' AND last_name = 'Marc';
+-- SELECT * FROM students WHERE first_name = 'Benichou' OR last_name = 'Marc';
+-- SELECT * FROM students WHERE last_name LIKE '%a%';
+-- SELECT * FROM students WHERE last_name LIKE '%a';
+-- SELECT * FROM students WHERE last_name ILIKE 'a%';
+-- SELECT * FROM students WHERE SUBSTRING(last_name FROM LENGTH(last_name) - 1 FOR 1) = 'a';
+-- SELECT * FROM students WHERE birth_date > '200-01-01'
+
+
+
+
+	
